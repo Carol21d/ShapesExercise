f51c2877603c27257a06737761bfaeb481ee7bb2
blake S <design.harls@icloud.com>
2022-12-28 16:29:26 +0100

    add repo and structure

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+
+// function hideFigures() {
+//     let figures = document.getElementById("hideElementTriangle");
+//     if (figures.style.display === "none") {
+//         figures.style.display = "block";
+        
+//     }else{
+//         figures.style.display = "none";
+//     }
+    
+// }
+
+
+
+
+ function hideFigures() {
+
+        
+     }
+
+
+
+
+document.getElementById("hideElementTriangle").onclick = function(){
+    document.getElementById("hideElementTriangle").style.display = "none";
+}
+
+document.getElementById("hideElementSquare").onclick = function(){
+    document.getElementById("hideElementSquare").style.display = "none";
+}
+
+document.getElementById("hideElementCircle").onclick = function(){
+    document.getElementById("hideElementCircle").style.display = "none";
+}
